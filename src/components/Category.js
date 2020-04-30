346402f2d8d375b6c4256ca8fa4422231f0a31bb
eelcire <eelcire.eelcire@hotmail.com>
2020-04-30 14:03:27 -0400

    jeopardy

---
+++ src/components/Category.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Clue from './Clue';
+
+class Category extends Component {
+  state = {
+    clues: [],
+  };
+
+  componentDidMount() {
+    fetch(`http://jservice.io/api/clues?category=${this.props.category.id}`)
+      .then((res) => res.json())
+      .then((json) => this.setState({ clues: json }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Link className="link-home" to="/">
+          <h4>Home</h4>
+        </Link>
+        <h2>{this.props.category.title}</h2>
+        {this.state.clues.map((clue) => {
+          return <Clue key={clue.id} clue={clue} />;
+        })}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { category: state.category };
+}
+
+export default connect(mapStateToProps, null)(Category);
